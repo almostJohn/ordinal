9693764643e4c1a55de458586aa2b2f4409e7fb7
John <109547015+subtlepoem@users.noreply.github.com>
2022-07-21 11:43:44 +0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+interface Suffix {
+  zero?: string;
+  one: string;
+  two: string;
+  few: string;
+  many?: string;
+  other: string;
+}
+
+export function ordinal(count: number) {
+  const pluralRules = new Intl.PluralRules('en', {
+    type: 'ordinal',
+  });
+
+  const suffixes: Suffix = {
+    one: 'st',
+    two: 'nd',
+    few: 'rd',
+    other: 'th',
+  }
+
+  const ordinalExt = suffixes[pluralRules.select(count)];
+  return (count + ordinalExt);
+}
